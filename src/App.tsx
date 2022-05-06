61bb792bfebb9db67015af84fe770bced15c66f0
Nanda <mariafernanda-coro@hotmail.com>
2022-05-05 22:30:34 -0300

    Utilizando hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useFetch } from "./hooks/useFetch";
+
+type Repository = {
+  full_name: string;
+  description: string;
+};
 
 function App() {
+  const { data: repositories, isFetching } = useFetch<Repository[]>(
+    "users/diego3g/repos",
+    {}
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {isFetching && <p>Carregando...</p>}
+      {repositories?.map((repo) => {
+        return (
+          <li key={repo.full_name}>
+            <strong>{repo.full_name}</strong>
+            <p>{repo.description}</p>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
 
